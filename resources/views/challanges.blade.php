4bc705b966a82160d017a9a830bd1a467ccdf71b
shashi <ranjanthakur1996@gmail.com>
2021-08-19 14:11:03 +0530

    add challange page

--- resources/views/challanges.blade.php
+++ resources/views/challanges.blade.php
@@ -728,65 +728,8 @@
     <!-- /Side navigation -->
     <!-- Back To Top Button -->
     <div id="backtotop"><a href="#"></a></div>
-    <div id="style-switcher" class="style-switcher visible">
-        <div class="switcher-close">
-            <i class="material-icons">close</i>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="core" name="theme_selector" checked>
-            <div class="style-dot-inner"></div>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="purple" name="theme_selector">
-            <div class="style-dot-inner is-purple"></div>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="teal" name="theme_selector">
-            <div class="style-dot-inner is-teal"></div>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="green" name="theme_selector">
-            <div class="style-dot-inner is-green"></div>
-        </div>
-
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="azur" name="theme_selector">
-            <div class="style-dot-inner is-azur"></div>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="blue" name="theme_selector">
-            <div class="style-dot-inner is-blue"></div>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="night" name="theme_selector">
-            <div class="style-dot-inner is-night"></div>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="yellow" name="theme_selector">
-            <div class="style-dot-inner is-yellow"></div>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="orange" name="theme_selector">
-            <div class="style-dot-inner is-orange"></div>
-        </div>
-        <!--Main Theme-->
-        <div class="style-dot">
-            <input type="radio" id="red" name="theme_selector">
-            <div class="style-dot-inner is-red"></div>
-        </div>
-    </div> <!-- Bulchat Button -->
-    <div id="bulchat" class="open">
-        <div class="chat-button open g-item"></div>
-    </div> <!-- Chat widget -->
+    <!-- Bulchat Button -->
+    <!-- Chat widget -->
     <div id="chat-widget">
         <div class="chat-widget-body is-closed">
             <div class="chat-header">
